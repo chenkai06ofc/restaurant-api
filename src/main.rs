7181b80e978418d6d4509936fa915550588b125a
Chen Kai <cako@Chens-MacBook-Air.local>
2021-07-08 18:42:17 +0900

    api skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Request, Body, Response, StatusCode, Method, Server};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let server = Server::bind(&addr).serve(make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(service_fn(handle_req))
+    }));
+    println!("Listening on http://{}", addr);
+    server.await?;
+    Ok(())
+}
+
+async fn handle_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/item/add") => {
+            Ok(Response::new("add succeed".into()))
+        }
+        (&Method::POST, "/item/remove") => {
+            Ok(Response::new("remove succeed".into()))
+        }
+        (&Method::GET, "/item/query") => {
+            Ok(Response::new("query succeed".into()))
+        }
+        _ => Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap())
+    }
 }
