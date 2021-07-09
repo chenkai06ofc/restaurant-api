bd18ff631a48987ac5c95b75b5ff0242dd8d90e5
Chen Kai <cako@Chens-MacBook-Air.local>
2021-07-09 19:00:03 +0900

    extract handler functions

--- src/main.rs
+++ src/main.rs
@@ -39,56 +39,25 @@ async fn handle_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
         (&Method::POST, "/item/add") => {
             match (params.get("table_no"), params.get("content")) {
                 (Some(table_no), Some(content)) => unsafe {
-                    let item_no = next_item_no().to_string();
-
-                    // HashMap table:{table_no}
-                    let table_key = format!("table:{}", table_no);
-                    let _ : () = con.hset(&table_key, &item_no, content).unwrap();
-
-                    // HashMap item:{table_no-item_no}
-                    let item_key = format!("item:{}-{}", table_no, item_no);
-                    let prepare_time_min = rand::thread_rng().gen_range(5..15);
-                    let time_str = prepare_time_min.to_string();
-                    let _ : () = con.hset_multiple(
-                        &item_key,
-                        &[("content", content), ("prepare_time_min", &time_str) ])
-                        .unwrap();
-
+                    add_item(&mut con, table_no, content);
                     Ok(Response::new("add succeed".into()))
                 }
-                _ => Ok(Response::builder()
-                    .status(StatusCode::BAD_REQUEST)
-                    .body(Body::from("Please specify table_no and content"))
-                    .unwrap())
+                _ => Ok(bad_request("Please specify table_no & content"))
             }
         }
         (&Method::POST, "/item/remove") => {
             match (params.get("table_no"), params.get("item_no")) {
                 (Some(table_no), Some(item_no)) => {
-                    let mut pipe = redis::pipe();
-                    let table_key = format!("table:{}", table_no);
-                    let item_key = format!("item:{}-{}", table_no, item_no);
-                    let _ : () = pipe.hdel(&table_key, item_no).del(&item_key).query(&mut con).unwrap();
+                    remove_item(&mut con, table_no, item_no);
                     Ok(Response::new("remove succeed".into()))
                 }
-                _ => Ok(Response::builder()
-                    .status(StatusCode::BAD_REQUEST)
-                    .body(Body::from("Please specify table_no and content"))
-                    .unwrap())
+                _ => Ok(bad_request("Please specify table_no & item_no"))
             }
         }
         (&Method::GET, "/item/query") => {
             match (params.get("table_no"), params.get("item_no")) {
                 (Some(table_no), Some(item_no)) => {
-                    let map : HashMap<String, String> =
-                        con.hgetall(format!("item:{}-{}", table_no, item_no)).unwrap();
-
-                    let item = Item {
-                        table_no: table_no.parse().unwrap(),
-                        item_no: item_no.parse().unwrap(),
-                        content: String::from(map.get("content").unwrap()),
-                        prepare_time_min: map.get("prepare_time_min").unwrap().parse().unwrap()
-                    };
+                    let item = get_item(&mut con, table_no, item_no);
                     let serialized = serde_json::to_string(&item).unwrap();
                     Ok(Response::new(serialized.into()))
                 }
@@ -97,18 +66,62 @@ async fn handle_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> 
                     let serialized = serde_json::to_string(&map).unwrap();
                     Ok(Response::new(serialized.into()))
                 }
-                _ => Ok(Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(Body::empty())
-                    .unwrap())
+                _ => Ok(not_found())
             }
 
         }
-        _ => Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::empty())
-                .unwrap())
+        _ => Ok(not_found())
     }
+}
+
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::empty())
+        .unwrap()
+}
+
+fn bad_request(msg: &'static str) -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::from(msg))
+        .unwrap()
+}
+
+unsafe fn add_item(con: &mut Connection, table_no: &String, content: &String) {
+    let item_no = next_item_no().to_string();
+
+    // HashMap table:{table_no}
+    let table_key = format!("table:{}", table_no);
+    let _ : () = con.hset(&table_key, &item_no, content).unwrap();
+
+    // HashMap item:{table_no-item_no}
+    let item_key = format!("item:{}-{}", table_no, item_no);
+    let prepare_time_min = rand::thread_rng().gen_range(5..15);
+    let time_str = prepare_time_min.to_string();
+    let _ : () = con.hset_multiple(
+        &item_key,
+        &[("content", content), ("prepare_time_min", &time_str) ])
+        .unwrap();
+}
+
+fn get_item(con: &mut Connection, table_no: &String, item_no: &String) -> Item {
+    let map : HashMap<String, String> =
+        con.hgetall(format!("item:{}-{}", table_no, item_no)).unwrap();
+
+    Item {
+        table_no: table_no.parse().unwrap(),
+        item_no: item_no.parse().unwrap(),
+        content: String::from(map.get("content").unwrap()),
+        prepare_time_min: map.get("prepare_time_min").unwrap().parse().unwrap()
+    }
+}
+
+fn remove_item(con: &mut Connection, table_no: &String, item_no: &String) {
+    let mut pipe = redis::pipe();
+    let table_key = format!("table:{}", table_no);
+    let item_key = format!("item:{}-{}", table_no, item_no);
+    let _ : () = pipe.hdel(&table_key, item_no).del(&item_key).query(con).unwrap();
 }
 
 #[derive(Serialize, Deserialize)]
