b20d4cd76e9f3ded8c619e509d05959d74a52270
Chen Kai <cako@Chens-MacBook-Air.local>
2021-07-12 16:04:53 +0900

    modify parse_query_str

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,8 @@
 use std::collections::HashMap;
 
 pub fn parse_query_str(query_string: &str) -> HashMap<String, String> {
-    let mut params: HashMap<String, String> = HashMap::new();
-    let p = &mut params;
-    query_string.split("&").for_each(|s| {
-        let (key, value) = s.split_once("=").unwrap();
-        p.insert(String::from(key), String::from(value));
-    });
-    return params;
+    query_string.split("&")
+        .map(|s| s.split_once("=").unwrap())
+        .map(|(s1, s2)| (String::from(s1), String::from(s2)))
+        .collect()
 }
